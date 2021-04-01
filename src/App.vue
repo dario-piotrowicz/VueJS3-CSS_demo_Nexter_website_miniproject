33b100c8f9d085d0d4dcbb1b3e0103ac29497357
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-01 18:49:58 +0100

    basic style settings

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,24 @@ export default {
 </script>
 
 <style lang="scss">
+html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+body {
+  font-family: "Nunito", sans-serif;
+  color: #6d5d4b;
+  font-weight: 300;
+  line-height: 1.6;
+}
+
 #app {
 }
 </style>
