3659d9cf06d653ccf0352d86718bafb92f60d8ad
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-01 21:15:30 +0100

    applied the general grid system and placed the side-bar

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,3 +1,10 @@
 <template>
   <div class="side-bar">side-bar</div>
 </template>
+
+<style lang="scss" scoped>
+.side-bar {
+  grid-column: sidebar-start / sidebar-end;
+  grid-row: 1 / -1;
+}
+</style>
