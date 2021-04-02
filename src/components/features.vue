e7fd0724c3c072b402f1d4165d9bcc965d7dcd58
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-02 10:29:44 +0100

    added content to features section

--- src/components/features.vue
+++ src/components/features.vue
@@ -1,13 +1,61 @@
 <template>
   <section class="features">
-    <div class="feature">feature 1</div>
-    <div class="feature">feature 2</div>
-    <div class="feature">feature 3</div>
-    <div class="feature">feature 4</div>
-    <div class="feature">feature 5</div>
-    <div class="feature">feature 6</div>
+    <div v-for="feature of features" :key="feature.title" class="feature">
+      <svg class="feature__icon">
+        <use :xlink:href="`#${feature.iconId}`"></use>
+      </svg>
+      <h4 class="feature__title">{{ feature.title }}</h4>
+      <p class="feature__text">{{ feature.text }}</p>
+    </div>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      features: [
+        {
+          iconId: "icon-global",
+          title: "World's best luxury homes",
+          text:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.",
+        },
+        {
+          iconId: "icon-trophy",
+          title: "Only the best properties",
+          text:
+            "Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.",
+        },
+        {
+          iconId: "icon-map-pin",
+          title: "All homes in top locations",
+          text:
+            "Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.",
+        },
+        {
+          iconId: "icon-key",
+          title: "New home in one week",
+          text:
+            "Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          iconId: "icon-presentation",
+          title: "Top 1% realtors",
+          text:
+            "Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.",
+        },
+        {
+          iconId: "icon-lock",
+          title: "Secure payments on Nexter",
+          text:
+            "Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.",
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .features {
