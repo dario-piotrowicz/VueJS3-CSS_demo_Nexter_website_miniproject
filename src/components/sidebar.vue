1dabe04f3c17acf0246d6cff0ba53173b4bdda41
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-02 11:23:44 +0100

    styled the sidebar

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="side-bar">side-bar</div>
+  <div class="side-bar"></div>
 </template>
 
 <style lang="scss" scoped>
 .side-bar {
+  background-color: #c69963;
   grid-column: sidebar-start / sidebar-end;
   grid-row: 1 / -1;
 }
