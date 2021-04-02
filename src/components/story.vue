2896cdce2d87f631e70e8adc731adee8f01534ef
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-02 15:40:21 +0100

    implemented the story pictures

--- src/components/story.vue
+++ src/components/story.vue
@@ -1,6 +1,17 @@
 <template>
   <section class="story">
-    <div class="story__pictures"></div>
+    <div class="story__pictures">
+      <img
+        src="../assets/images/story/couple.jpeg"
+        alt="Couple"
+        class="story__picture-couple"
+      />
+      <img
+        src="../assets/images/story/house.jpeg"
+        alt="House"
+        class="story__picture-house"
+      />
+    </div>
     <div class="story__content">
       <h3 class="story__subtitle">Happy Customers</h3>
       <h2 class="story__title">&ldquo;The best decision of our lives&rdquo;</h2>
@@ -23,11 +34,36 @@
   display: flex;
 
   &__pictures {
-    flex: 1;
+    flex: 0 0 50%;
+    background-image: linear-gradient(
+        rgba($primary-color, 0.5),
+        rgba($primary-color, 0.5)
+      ),
+      url("../assets/images/story/background.jpg");
+
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(6, 1fr);
+    align-items: center;
+  }
+
+  &__picture-couple {
+    width: 100%;
+    grid-row: 2 / 6;
+    grid-column: 2 / 6;
+    box-shadow: 0 2rem 5rem transparentize(#000, 0.9);
+  }
+
+  &__picture-house {
+    width: 115%;
+    grid-row: 4 / 6;
+    grid-column: 4 / 7;
+    z-index: 2;
+    box-shadow: 0 2rem 5rem transparentize(#000, 0.8);
   }
 
   &__content {
-    flex: 1;
+    flex: 0 0 50%;
     background-color: #f9f7f6;
     padding: 6rem 8vw;
 
