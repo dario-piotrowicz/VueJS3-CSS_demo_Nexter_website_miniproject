d211dfd6b6907d7a767583cbfda857ac87a26bff
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-02 10:00:43 +0100

    defined the basic grid stracture for the features section

--- src/components/features.vue
+++ src/components/features.vue
@@ -1,9 +1,22 @@
 <template>
-  <section class="features">features</section>
+  <section class="features">
+    <div class="feature">feature 1</div>
+    <div class="feature">feature 2</div>
+    <div class="feature">feature 3</div>
+    <div class="feature">feature 4</div>
+    <div class="feature">feature 5</div>
+    <div class="feature">feature 6</div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
 .features {
   grid-column: center-start / center-end;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 6rem;
+
+  margin: 15rem 0;
 }
 </style>
