1be2d4748a29adec33f8579dbd236dad946b9ad3
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-04-02 11:18:02 +0100

    styled the features

--- src/components/features.vue
+++ src/components/features.vue
@@ -62,9 +62,37 @@ export default {
   grid-column: center-start / center-end;
 
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
   gap: 6rem;
+  align-items: start;
 
   margin: 15rem 0;
+}
+
+.feature {
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  row-gap: 1.5rem;
+  column-gap: 2.5rem;
+
+  &__icon {
+    fill: #c69963;
+    $size: 4.5rem;
+    width: $size;
+    height: $size;
+    grid-row: 1 / span 2;
+    transform: translateY(-1rem);
+  }
+
+  &__title {
+    font-family: "Josefin Sans", sans-serif;
+    font-weight: 400;
+    font-size: 1.9rem;
+    color: #54483a;
+  }
+
+  &__text {
+    font-size: 1.7rem;
+  }
 }
 </style>
